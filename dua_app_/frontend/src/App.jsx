5919a70a675497d12f2c54ea9fa628ba71d22b01
Md.Ashraful Islam <crackthecode10@gmail.com>
2025-04-16 02:17:12 +0600

    React App initialize complete

---
+++ dua_app_/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import DuaPage from './pages/DuaPage'
+
+function App() {
+  
+  return (
+    <div className='p-4'>
+      <DuaPage/>
+    </div>
+    
+  )
+}
+
+export default App
